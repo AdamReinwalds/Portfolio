3a7f74dbf1a8cae31f304d46e0042b4adbb34d8b
AdamReinwalds <adam.reinvalds@gmail.com>
2025-02-04 13:02:46 +0100

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/Portfolio/", // Replace with your GitHub repo name
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      input: "./index.html", // Ensure this is the only entry point
+    },
+  },
 });
