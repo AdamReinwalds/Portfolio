ee65026ff1f910fcc11ed6ce15ed8470514e5c5b
AdamReinwalds <adam.reinvalds@gmail.com>
2025-02-10 20:20:37 +0100

    fixed hamburger-menu

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links")
-    const icon = document.querySelector(".hamburger-icon")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+window.toggleMenu = toggleMenu;
