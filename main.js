190f7f6e61b3f8d4f772c3386e3bbf9df91c7c6d
AdamReinwalds <adam.reinvalds@gmail.com>
2025-03-26 12:59:08 +0100

    Replaced hrefs with js

--- main.js
+++ main.js
@@ -6,4 +6,13 @@ function toggleMenu() {
 }
 
 document.querySelector(".hamburger-icon").addEventListener("click", toggleMenu);
-document.querySelector(".menu-links a").addEventListener("click", toggleMenu);
+document
+  .querySelectorAll(".menu-links a")
+  .forEach((el) => el.addEventListener("click", toggleMenu));
+
+function scrollToElementId(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth",
+  });
+  console.log("scrolling to", id);
+}
